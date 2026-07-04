a8ade896a46f1647cb15b827ba1b3b1130338278
Akshay B <akshay2642005@gmail.com>
2026-07-04 15:48:01 +0530

    Update: find-duplicate-integer - submission-1

--- Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -1,35 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode();
-        ListNode* current = dummy;
-        int carry = 0;
-        while (l1 != nullptr || l2 != nullptr || carry !=0 ) {
-            int v1 = (l1 != nullptr) ? l1->val : 0;
-            int v2 = (l2 != nullptr) ? l2->val : 0;
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
 
-            int new_value = v1 + v2 + carry;
-            carry = new_value / 10;
-            new_value = new_value % 10;
-            current->next = new ListNode(new_value);
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
 
-            current = current->next;
-            l1 = (l1 != nullptr) ? l1->next: nullptr;
-            l2 = (l2 != nullptr) ? l2->next: nullptr;
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        ListNode* result = dummy->next;
-        delete dummy;
-        return result;
+        return slow;
     }
 };
