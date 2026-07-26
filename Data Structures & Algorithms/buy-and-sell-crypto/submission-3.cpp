5b148b50a11ff708e58c3c20f74457b614375699
Akshay B <akshay2642005@gmail.com>
2026-07-26 12:31:30 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0, right = 1, maxProfit = 0;
+        while ( right < prices.size()) {
+            if (prices[left] < prices[right]) {
+                maxProfit = max(maxProfit, (prices[right] - prices[left]));
+            }
+            else {
+                left = right;
+            }
+            right++;
+        }
+        return maxProfit;
+    }
+};
