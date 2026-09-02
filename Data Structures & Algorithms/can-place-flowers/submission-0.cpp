102c030c3ecce1f5deb64222874177232e556561
Akshay B <akshay2642005@gmail.com>
2026-09-02 11:48:13 +0530

    Add: can-place-flowers - submission-0

---
+++ Data Structures & Algorithms/can-place-flowers/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int empty = flowerbed[0] == 0 ? 1 : 0;
+
+        for (int f : flowerbed) {
+            if (f == 1) {
+                n = n - (empty - 1) / 2;
+                empty = 0;
+            } else {
+                empty++;
+            }
+        }
+        n = n - empty / 2;
+        return n <= 0;
+    }
+};
