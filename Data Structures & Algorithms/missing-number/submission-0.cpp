72b1ec9a71000f19bce9f51160e685814d3c43b5
Akshay B <akshay2642005@gmail.com>
2026-09-16 07:47:27 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum1 = nums.size();
+        int sum2 = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            sum1 += i;
+            sum2 += nums[i];
+        }
+        return sum1 - sum2;
+    }
+};
